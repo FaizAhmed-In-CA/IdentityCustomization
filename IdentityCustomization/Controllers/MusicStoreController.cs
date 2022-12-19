378ed48d2514a9315557b265c29da32bb34889c4
Faiz Ahmed <bdfaiz@gmail.com>
2022-12-19 20:11:48 +0600

    Initial commit

---
+++ IdentityCustomization/Controllers/MusicStoreController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdentityCustomization.Controllers
+{
+    public class MusicStoreController : Controller
+    {
+        // GET: MusicStore
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome()
+        {
+            return "Hello, this is welcome action message";
+        }
+
+
+    }
+}
---
+++ IdentityCustomization/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IdentityCustomization.MvcApplication" Language="C#" %>
